41c1878f82d2a6d64a1e7400b41c0073ca1f25f1
dzhv <dz.vysniauskas@gmail.com>
2019-02-09 11:49:47 +0000

    Restructuring some code for preprocessing

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -2,13 +2,27 @@ import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import os
 
-def create_x_dataframe():
-    return pd.DataFrame(dtype=np.float64, columns=[
-        "min", "max", "mean", "std", "q25", "q75", "iqr", "q01", "q99",
-        "min_50", "max_50", "mean_50", "std_50", "q25_50", "q75_50", "iqr_50", "q01_50", "q99_50",
-        "min_100", "max_100", "mean_100", "std_100", "q25_100", "q75_100", "iqr_100", "q01_100", "q99_100",
-        "min_1000", "max_1000", "mean_1000", "std_1000", "q25_1000", "q75_1000", "iqr_1000", "q01_1000", "q99_1000"
-    ])
+def feature_name_for_window(feature, window):
+    if window == None:
+        return feature
+    return "{0}_{1}".format(feature, window)
+
+def feature_names_for_windows(feature, windows):
+    return list(map(lambda window: feature_name_for_window(feature, window), windows))
+
+def flatten(l):
+    return [item for sublist in l for item in sublist]
+
+def get_columns(windowed_features, window_sizes, other_features):
+    return flatten(
+        list(
+            map(
+                lambda feature: feature_names_for_windows(feature, window_sizes), windowed_features)
+            )
+        ) + other_features
+
+def create_x_dataframe(columns):
+    return pd.DataFrame(dtype=np.float64, columns=columns)
         
 def create_y_dataframe(times=None):
     if times is None:
@@ -23,7 +37,7 @@ def calculate_statistical_features(data_chunk, window_size=None):
             "min": [data_chunk.acoustic_data.min()],
             "max": [data_chunk.acoustic_data.max()],
             "mean": [data_chunk.acoustic_data.mean()],
-            "std": [data_chunk.acoustic_data.std()],
+            "var": [data_chunk.acoustic_data.var()],
             "q25": [Q25],
             "q75": [Q75],
             "q01": [data_chunk.acoustic_data.quantile(0.01)],
@@ -31,57 +45,61 @@ def calculate_statistical_features(data_chunk, window_size=None):
             "iqr": [Q75 - Q25]
         }
     else:
-        Q75 = data_chunk.acoustic_data.rolling(window=window_size).quantile(0.75).mean()
-        Q25 = data_chunk.acoustic_data.rolling(window=window_size).quantile(0.25).mean()
+        windows = data_chunk.acoustic_data.rolling(window=window_size)
+        Q75 = windows.quantile(0.75).mean()
+        Q25 = windows.quantile(0.25).mean()
         return {
-            "min_" + str(window_size): [data_chunk.acoustic_data.rolling(window=window_size).min().mean()],
-            "max_" + str(window_size): [data_chunk.acoustic_data.rolling(window=window_size).max().mean()],
-            "mean_" + str(window_size): [data_chunk.acoustic_data.rolling(window=window_size).mean().mean()],
-            "std_" + str(window_size): [data_chunk.acoustic_data.rolling(window=window_size).std().mean()],
-            "q25_" + str(window_size): [Q25],
-            "q75_" + str(window_size): [Q75],
-            "q01_" + str(window_size): [data_chunk.acoustic_data.rolling(window=window_size).quantile(0.01).mean()],
-            "q99_" + str(window_size): [data_chunk.acoustic_data.rolling(window=window_size).quantile(0.99).mean()],
-            "iqr_" + str(window_size): [Q75 - Q25]
+            feature_name_for_window("min", window_size): [windows.min().mean()],
+            feature_name_for_window("max", window_size): [windows.max().mean()],
+            feature_name_for_window("mean", window_size): [windows.mean().mean()],
+            feature_name_for_window("var", window_size): [windows.var().mean()],
+            feature_name_for_window("q25", window_size): [Q25],
+            feature_name_for_window("q27", window_size): [Q75],
+            feature_name_for_window("q01", window_size): [windows.quantile(0.01).mean()],
+            feature_name_for_window("q99", window_size): [windows.quantile(0.99).mean()],
+            feature_name_for_window("iqr", window_size): [Q75 - Q25]
         }
-        
 
 def process_data():
     sample_size = int(150e3)
+    windowed_features = ["min", "max", "mean", "var", "q25", "q75", "iqr", "q01", "q99"]
     window_sizes = [None, 50, 100, 1000]
+    other_features = []
+    columns = get_columns(windowed_features, window_sizes, other_features)
     train_sample_n = 3334   # We have about 629 million rows in total, so dedicating 3334 * 150 000  = 500 100 000 for training
     train_row_n = sample_size * train_sample_n
-        
-    samples_read = 0
-    x_train = create_x_dataframe()
-    x_val = create_x_dataframe()
+            
+    x_train = create_x_dataframe(columns)
+    x_val = create_x_dataframe(columns)
     y_train = create_y_dataframe()
     y_val = create_y_dataframe()
+
+    train_data_file = "../data/train.csv"
+    if not os.path.isfile(train_data_file):
+        print("train.csv file not found in data folder")
+        print("this file is not commited in the repo and needs to be added manually")
+        return (None, None, None, None)
     
-    for chunk in pd.read_csv("../input/train.csv", chunksize=sample_size):
+    chunks_processed = 0    
+    for chunk in pd.read_csv(train_data_file, chunksize=sample_size):
         if chunk.shape[0] < sample_size:        # throwing out last remaining rows
             break
         features = {}
         for window_size in window_sizes:
             features.update(calculate_statistical_features(chunk, window_size))
             
-        if samples_read < train_row_n:
+        if chunks_processed * sample_size < train_row_n:
             x_train = x_train.append(pd.DataFrame(data=features))
             y_train = y_train.append(create_y_dataframe(chunk.time_to_failure.tail(1).values))
         else:
             x_val = x_val.append(pd.DataFrame(data=features))
             y_val = y_val.append(create_y_dataframe(chunk.time_to_failure.tail(1).values))
         
-        samples_read += sample_size
-    
-        print(x_train)
-        print(y_train)
-        print(x_val)
-        print(y_val)
-    
+        chunks_processed += 1
 
+        if chunks_processed % 20 == 0:
+            print("{0} chunks processed, {1} remaining".format(chunks_processed, 4193 - chunks_processed))
 
     return x_train, y_train, x_val, y_val
 
 x_train, y_train, x_val, y_val = process_data()
-
