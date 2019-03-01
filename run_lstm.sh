b9942f568073ad0ba5ea904f07bd1170a46aa62f
dzhv <dz.vysniauskas@gmail.com>
2019-03-01 18:14:15 +0000

    correction for run_lstm script

--- run_lstm.sh
+++ run_lstm.sh
@@ -36,6 +36,6 @@ source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
 python scripts/experiments/lstm_experiment.py --data_path /home/${STUDENT_ID}/lanl_earthquake/data \
 											 --experiment_name "lstm_full_raw" \
 											 --segment_size 150000 --element_size 1000 \
-											 --use_gpu "true" --gpu_id "0,1,2,3" \
+											 --use_gpu "true" --gpu_id "0,1" \
 											 --num_epochs 60 --dropout 0.3 \
 											 --learning_rate 0.0002
