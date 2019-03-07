33bb2b096f359735676848d38b6de04bfe22b886
Michael S. Rizkalla <33562048+MichaelRizkalla@users.noreply.github.com>
2019-03-07 10:22:27 +0000

    Adding overlap argument

--- scripts/experiments/SeqDataProvider.py
+++ scripts/experiments/SeqDataProvider.py
@@ -5,7 +5,7 @@ import numpy as np
 class SeqDataProvider(DataProvider):
     # returns the data of shape:  (num_segments, segment_size / element_size, feature_size)
 
-    def __init__(self, data_path, which_set='train', segment_size=150000, element_size=1000, feature_size=58,
+    def __init__(self, data_path, which_set='train', segment_size=150000, element_size=1000, feature_size=58, overlap=False,
         batch_size=1, rng=None):
         assert which_set in ['train', 'val'], (
             'Expected which_set to be either train or val '
@@ -26,13 +26,27 @@ class SeqDataProvider(DataProvider):
         signal = np.loadtxt(file_path_x, delimiter=",", skiprows=1)        
         time = np.loadtxt(file_path_y, delimiter=",", skiprows=1)
         
-        data_length = signal.shape[0] // (segment_size // element_size) * (segment_size // element_size)
-        signal = signal[:data_length, :]
-        time = time[:data_length]
+        if overlap == False:
+            data_length = signal.shape[0] // (segment_size // element_size) * (segment_size // element_size)
+            signal = signal[:data_length, :]
+            time = time[:data_length]
         
-        n_signal = signal.shape[0]        
-        inputs = signal.reshape(n_signal // (segment_size // element_size), segment_size // element_size, feature_size)
-        targets = time[np.arange((segment_size // element_size) - 1, time.shape[0], (segment_size // element_size))]
+            n_signal = signal.shape[0]        
+            inputs = signal.reshape(n_signal // (segment_size // element_size), segment_size // element_size, feature_size)
+            targets = time[np.arange((segment_size // element_size) - 1, time.shape[0], (segment_size // element_size))]
+        
+        else:
+            overlap_percentage = 10
+            data_length = signal.shape[0] // (segment_size // element_size // overlap_percentage) * (segment_size // element_size // overlap_percentage)
+            signal = signal[:data_length, :]
+            time = time[:data_length]
+        
+            n_signal = int((signal.shape[0] / (segment_size // element_size // overlap_percentage))  - overlap_percentage)
+            inputs = np.zeros((n_signal, segment_size // element_size, feature_size))
+            targets = np.zeros(n_signal)
+            for i in range(n_signal):
+                inputs[i] = signal[(segment_size // element_size // overlap_percentage)*i:(segment_size // element_size)+((segment_size // element_size // overlap_percentage)*i)].reshape(segment_size // element_size, feature_size)
+                targets[i] = time[(segment_size // element_size)+((segment_size // element_size // overlap_percentage)*i)]
         
         print("inputs shape: {0}, targets shape: {1}".format(inputs.shape, targets.shape))
         assert inputs.shape[0] == targets.shape[0]
