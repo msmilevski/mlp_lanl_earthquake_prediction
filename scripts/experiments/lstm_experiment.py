20e05aa039525241691670b7143814d9fb42678c
Michael <s1817388@sms.ed.ac.uk>
2019-03-07 09:32:47 +0000

    changes

--- scripts/experiments/lstm_experiment.py
+++ scripts/experiments/lstm_experiment.py
@@ -1,4 +1,5 @@
 from mini_data_provider import MiniDataProvider
+from SeqDataProvider import SeqDataProvider
 from raw_data_provider import RawDataProvider
 from LSTM import LSTM
 import numpy as np
@@ -11,13 +12,13 @@ args = get_args()
 rng = np.random.RandomState(seed=args.seed)  # set the seeds for the experiment
 torch.manual_seed(seed=args.seed) # sets pytorch's seed
 
-train_data = RawDataProvider(which_set='train', data_path=args.data_path, segment_size=args.segment_size, 
-  element_size=args.element_size, rng=rng, downsampled=args.downsampled)
-val_data = RawDataProvider(which_set='val', data_path=args.data_path, segment_size=args.segment_size, 
-  element_size=args.element_size, rng=rng, downsampled=args.downsampled)
 
-model = LSTM(input_size = args.element_size, hidden_size = 100, output_size=1, 
-	sequence_len=args.segment_size // args.element_size)
+train_data = SeqDataProvider(which_set='train', data_path=args.data_path, segment_size=args.segment_size,
+  element_size=args.element_size, feature_size=58, rng=rng)
+val_data = SeqDataProvider(which_set='val', data_path=args.data_path, segment_size=args.segment_size, 
+  element_size=args.element_size, feature_size=58, rng=rng)
+
+model = LSTM(input_size = 58 , hidden_size = args.hidden_size, output_size=1, dropout=args.dropout, num_layers=args.num_layers)
 
 experiment = ExperimentBuilder(network_model=model,
                                     experiment_name=args.experiment_name,
@@ -26,6 +27,7 @@ experiment = ExperimentBuilder(network_model=model,
                                     use_gpu=args.use_gpu,
                                     train_data=train_data, 
                                     val_data=val_data,
-                                    gpu_id=args.gpu_id
+				    gpu_id=args.gpu_id
                                     )
+
 experiment_metrics = experiment.run_experiment()  # run experiment and return experiment metrics
