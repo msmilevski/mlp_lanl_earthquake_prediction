669e0d620348a771e7dd45a3929da774b4e3346a
dzhv <dz.vysniauskas@gmail.com>
2019-03-09 14:33:00 +0000

    Updates for svr

--- scripts/svr.py
+++ scripts/svr.py
@@ -17,7 +17,7 @@ x_train, y_train, x_val, y_val = data_provider.get_data()
 
 models_evaluated = 0
 Cs = np.arange(0.01, 5.2, 0.4)
-kernels = ['rbf', 'poly', 'sigmoid', 'linear']
+kernels = ['rbf', 'sigmoid', 'linear']
 epsilons = [1.0, 2.0, 0.1]
 total_num_models = len(kernels) * len(Cs) * len(epsilons)
 
@@ -25,7 +25,14 @@ print("starting evaluation of {0} models".format(total_num_models))
 for C in Cs:
     for kernel in kernels:
         for epsilon in epsilons:
-            print("MODEL {0}".format(models_evaluated))
+        	print("MODEL {0}".format(models_evaluated))
+
+        	if models_evaluated <= 7:   # Already have results for these models
+        		print("skipping this model")
+        		models_evaluated += 1
+        		continue
+
+            
             start = time.time()
 
             clf = SVR(kernel=kernel, C=C, epsilon=epsilon)
@@ -38,7 +45,7 @@ for C in Cs:
             write_to_results(result)
 
             end = time.time()
-            print("Model {0} evaluation finished, time elapsed: {1}".format(models_evaluated, start - end))
+            print("Model {0} evaluation finished, time elapsed: {1}".format(models_evaluated, end - start))
             models_evaluated +=1
 
             print("{0}/{1} models evaluated".format(models_evaluated, total_num_models))
