e599340a9ec1c99db0996aba9531e259d61bb449
dzhv <dz.vysniauskas@gmail.com>
2019-03-11 23:58:31 +0000

    one more experiment idea

--- scripts/experiments/lstm_experiment.py
+++ scripts/experiments/lstm_experiment.py
@@ -19,11 +19,11 @@ if args.overlapped_data:
     data_path = os.path.join(args.data_path, "only_train.csv")
     train_data = OverlappedDataProvider(data_filepath=data_path, chunk_size=args.segment_size, 
         batch_size=args.batch_size, overlap_fraction=args.overlap_fraction)
-    val_data_path = os.path.join(args.data_path, "only_val.csv")
-    val_data = OverlappedDataProvider(data_filepath=val_data_path, 
-        chunk_size=args.segment_size, batch_size=args.batch_size, overlap_fraction=args.overlap_fraction)
-    # val_data = RawDataProvider(which_set='val', data_path=args.data_path, segment_size=args.segment_size, 
-    #     element_size=args.element_size, rng=rng, batch_size=args.batch_size, mini=args.mini_data)
+    # val_data_path = os.path.join(args.data_path, "only_val.csv")
+    # val_data = OverlappedDataProvider(data_filepath=val_data_path, 
+    #     chunk_size=args.segment_size, batch_size=args.batch_size, overlap_fraction=args.overlap_fraction)
+    val_data = RawDataProvider(which_set='val', data_path=args.data_path, segment_size=args.segment_size, 
+        element_size=args.element_size, rng=rng, batch_size=args.batch_size, mini=args.mini_data)
 else:
     train_data = RawDataProvider(which_set='train', data_path=args.data_path, segment_size=args.segment_size, 
     element_size=args.element_size, rng=rng, batch_size=args.batch_size, mini=args.mini_data)
