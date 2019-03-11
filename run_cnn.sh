2ad3ff8df08adcf2086f207cd37d8faf27fa3622
Marko Smilevksi <m.smilevski57@gmail.com>
2019-03-11 12:04:22 +0000

    Change partition

--- run_cnn.sh
+++ run_cnn.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH -N 1	  # nodes requested
 #SBATCH -n 1	  # tasks requested
-#SBATCH --partition=LongJobs
+#SBATCH --partition=Standard
 #SBATCH --gres=gpu:2
 #SBATCH --mem=12000  # memory in Mb
 #SBATCH --time=0-30:00:00
