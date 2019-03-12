88df9c4d7ce8aa1ea279364b7f95c1b3a563e65e
dzhv <dz.vysniauskas@gmail.com>
2019-03-12 17:05:02 +0000

    gcloud experiment with larger batch size

--- gcloud_lstm.sh
+++ gcloud_lstm.sh
@@ -1,7 +1,7 @@
 python scripts/experiments/lstm_experiment.py --data_path ./data \
-											 --experiment_name "lstm_overlapped_batch_15" \
+											 --experiment_name "lstm_overlap_batch_64" \
 											 --segment_size 150000 --element_size 1000 \
 											 --use_gpu "true" \
-											 --num_epochs 100 --dropout 0.1 \
-											 --learning_rate 0.0002 --batch_size 15 \
-											 --num_layers 3 --overlapped_data "true"
+											 --num_epochs 100 --dropout 0.25 \
+											 --learning_rate 0.0005 --batch_size 64 \
+											 --num_layers 3 --overlapped_data "true" --overlap_fraction 0.9
