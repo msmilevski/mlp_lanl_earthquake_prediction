dc81f0146bf5f5dca734d436aba6eac5e1cf7ab6
dzhv <dz.vysniauskas@gmail.com>
2019-03-12 09:38:41 +0000

    gcloud experiment

--- gcloud_lstm.sh
+++ gcloud_lstm.sh
@@ -1,7 +1,7 @@
 python scripts/experiments/lstm_experiment.py --data_path ./data \
-											 --experiment_name "lstm_overlapped_is_baseline" \
+											 --experiment_name "lstm_train_overlap_05_dropout_05" \
 											 --segment_size 150000 --element_size 1000 \
 											 --use_gpu "true" \
-											 --num_epochs 100 --dropout 0 \
-											 --learning_rate 0.0002 --batch_size 1 \
-											 --num_layers 2
+											 --num_epochs 100 --dropout 0.5 \
+											 --learning_rate 0.0002 --batch_size 2 \
+											 --num_layers 3 --overlapped "true" --overlap_fraction 0.5 \ 
