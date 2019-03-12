681de92eeabb2c7c64043a6a793363120f67828f
dzhv <dz.vysniauskas@gmail.com>
2019-03-12 13:30:26 +0000

    this was supposed to be the last commit

--- gcloud_lstm.sh
+++ gcloud_lstm.sh
@@ -1,7 +1,7 @@
 python scripts/experiments/lstm_experiment.py --data_path ./data \
-											 --experiment_name "lstm_train_overlap_05_dropout_05" \
+											 --experiment_name "lstm_overlapped_batch_15" \
 											 --segment_size 150000 --element_size 1000 \
 											 --use_gpu "true" \
-											 --num_epochs 100 --dropout 0.5 \
-											 --learning_rate 0.0002 --batch_size 2 \
-											 --num_layers 3 --overlapped_data "true" --overlap_fraction 0.5
+											 --num_epochs 100 --dropout 0.1 \
+											 --learning_rate 0.0002 --batch_size 15 \
+											 --num_layers 3 --overlapped_data "true"
