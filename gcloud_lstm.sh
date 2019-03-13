9b27834bc6245e369677d61d2888042d592bf36d
dzhv <dz.vysniauskas@gmail.com>
2019-03-13 09:50:14 +0000

    gcloud exp

--- gcloud_lstm.sh
+++ gcloud_lstm.sh
@@ -2,6 +2,6 @@ python scripts/experiments/lstm_experiment.py --data_path ./data \
 											 --experiment_name "lstm_overlap_batch_64" \
 											 --segment_size 150000 --element_size 1000 \
 											 --use_gpu "true" \
-											 --num_epochs 100 --dropout 0.25 \
-											 --learning_rate 0.0005 --batch_size 64 \
+											 --num_epochs 100 --dropout 0 \
+											 --learning_rate 0.001 --batch_size 64 \
 											 --num_layers 3 --overlapped_data "true" --overlap_fraction 0.9
