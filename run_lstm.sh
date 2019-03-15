1aac9ba3ef7bcdca676159b41b7bffac0a48fa35
dzhv <dz.vysniauskas@gmail.com>
2019-03-15 14:51:03 +0000

    experiment for non overlapped data

--- run_lstm.sh
+++ run_lstm.sh
@@ -2,7 +2,7 @@
 #SBATCH -N 1	  # nodes requested
 #SBATCH -n 1	  # tasks requested
 #SBATCH --partition=Standard
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:4
 #SBATCH --mem=12000  # memory in Mb
 #SBATCH --time=0-08:00:00
 
@@ -29,18 +29,18 @@ export DATASET_DIR=${TMP}data
 mkdir -p ${TMP}
 mkdir -p ${DATASET_DIR}
 
-echo "Copying dataset."
-rsync -ua /home/${STUDENT_ID}/lanl_earthquake/data/only_train.csv $DATASET_DIR
-rsync -ua /home/${STUDENT_ID}/lanl_earthquake/data/only_val.csv $DATASET_DIR
-echo "DATASET_DIR: $DATASET_DIR"
+# echo "Copying dataset."
+# rsync -ua /home/${STUDENT_ID}/lanl_earthquake/data/only_train.csv $DATASET_DIR
+# rsync -ua /home/${STUDENT_ID}/lanl_earthquake/data/only_val.csv $DATASET_DIR
+# echo "DATASET_DIR: $DATASET_DIR"
 
 # Activate the relevant virtual environment:
 
 source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
-python scripts/experiments/lstm_experiment.py --data_path ${DATASET_DIR} \
-											 --experiment_name "lstm_new_overlapped_provider_2" \
+python scripts/experiments/lstm_experiment.py --data_path /home/${STUDENT_ID}/lanl_earthquake/data \
+											 --experiment_name "lstm_fulL_raw3" \
 											 --segment_size 150000 --element_size 1000 \
-											 --use_gpu "true" --gpu_id "0,1" \
-											 --num_epochs 100 --dropout 0.1 \
-											 --learning_rate 0.001 --batch_size 64 \
-											 --num_layers 3 --overlapped_data "true" --overlap_fraction 0.9
+											 --use_gpu "true" --gpu_id "0,1,2,3" \
+											 --num_epochs 100 --dropout 0.5 \
+											 --learning_rate 0.0003 --batch_size 1 \
+											 --num_layers 3 --overlapped_data "false"
