638d667aea6700166c1572dcb884c2c3a447f3fb
dzhv <dz.vysniauskas@gmail.com>
2019-03-20 19:14:35 +0000

    svr test evaluation

--- scripts/data_provider.py
+++ scripts/data_provider.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def get_data():
+def get_overlapped_data():
 	x_train = pd.read_csv("data/new_x_train.csv")
 	y_train = pd.read_csv("data/new_y_train.csv", header=None)
 	x_val = pd.read_csv("data/new_x_val.csv")
@@ -8,5 +8,13 @@ def get_data():
 
 	return (x_train, y_train, x_val, y_val)
 
+def get_data():
+	x_train = pd.read_csv("data/x_train.csv")
+	y_train = pd.read_csv("data/y_train.csv", header=None)
+	x_val = pd.read_csv("data/new_x_val.csv")
+	y_val = pd.read_csv("data/new_y_val.csv", header=None)
+
+	return (x_train, y_train, x_val, y_val)
+
 def get_test_x():
-	return pd.read_csv("data/new_x_test.csv", header=None)
+	return pd.read_csv("data/new_x_test.csv")
--- scripts/svr.py
+++ scripts/svr.py
@@ -13,59 +13,82 @@ def write_to_results(text, mode="a"):
     with open(results_path, mode) as f:
         f.write(text + "\n")
 
-x_train, y_train, x_val, y_val = data_provider.get_data()
+x_train, y_train, x_val, y_val = data_provider.get_overlapped_data()
 
-models_evaluated = 0
-Cs = np.arange(0.01, 5.2, 0.4)
-kernels = ['rbf', 'sigmoid', 'linear']
-epsilons = [1.0, 2.0, 0.1]
-total_num_models = len(kernels) * len(Cs) * len(epsilons)
 
-def get_error_bar(errors):
-    errors = np.array(errors)
-    mean = np.mean(errors)
-    std = np.std(errors)
-    n = len(errors)
+def do_shit():
+    models_evaluated = 0
+    Cs = np.arange(0.01, 5.2, 0.4)
+    kernels = ['rbf', 'sigmoid', 'linear']
+    epsilons = [1.0, 2.0, 0.1]
+    total_num_models = len(kernels) * len(Cs) * len(epsilons)
 
-    return mean, std / np.sqrt(n)
+    def get_error_bar(errors):
+        errors = np.array(errors)
+        mean = np.mean(errors)
+        std = np.std(errors)
+        n = len(errors)
 
-models = [
-    (0.41, 'linear', 1),
-    (0.01, 'linear', 1),
-    (0.01, 'linear', 2)
-]
-seeds = [123131, 856856, 293420]#, 775241, 5562960] 
+        return mean, std / np.sqrt(n)
 
-print("starting evaluation of 10 models")
+    models = [
+        (0.41, 'linear', 1),
+        (0.01, 'linear', 1),
+        (0.01, 'linear', 2)
+    ]
+    seeds = [123131, 856856, 293420]#, 775241, 5562960] 
 
-# for C in Cs:
-#     for kernel in kernels:
-#         for epsilon in epsilons:
-for C, kernel, epsilon in models:
-    errors = []    
-    for seed in seeds:
-        print("MODEL {0}".format(models_evaluated))        
+    print("starting evaluation of 10 models")
 
-        start = time.time()
+    # for C in Cs:
+    #     for kernel in kernels:
+    #         for epsilon in epsilons:
+    for C, kernel, epsilon in models:
+        errors = []    
+        for seed in seeds:
+            print("MODEL {0}".format(models_evaluated))        
 
-        clf = SVR(kernel=kernel, C=C, epsilon=epsilon)
-        clf.fit(x_train, y_train)
-        predictions = clf.predict(x_val)
-        error = mean_absolute_error(y_val, predictions)
-        errors.append(error)
+            start = time.time()
 
-        
-        
-        # write_to_results(result)
-        result = 'Kernel: ' + kernel +', C: ' + str(C) + ', epsilon: ' + str(epsilon) +', MAE: ' + str(error)
-        print(result)
+            clf = SVR(kernel=kernel, C=C, epsilon=epsilon)
+            clf.fit(x_train, y_train)
+            predictions = clf.predict(x_val)
+            error = mean_absolute_error(y_val, predictions)
+            errors.append(error)
 
-        end = time.time()
-        print("Model {0} evaluation finished, time elapsed: {1}".format(models_evaluated, end - start))
-        models_evaluated +=1
+            
+            
+            # write_to_results(result)
+            result = 'Kernel: ' + kernel +', C: ' + str(C) + ', epsilon: ' + str(epsilon) +', MAE: ' + str(error)
+            print(result)
 
-        print("{0}/{1} models evaluated".format(models_evaluated, total_num_models))
+            end = time.time()
+            print("Model {0} evaluation finished, time elapsed: {1}".format(models_evaluated, end - start))
+            models_evaluated +=1
 
-    mean, error_bar = get_error_bar(errors)
-    model = 'Kernel: ' + kernel +', C: ' + str(C) + ', epsilon: ' + str(epsilon)
-    write_to_results("{0}, error: {1} +- {2}".format(model, mean, error_bar))
+            print("{0}/{1} models evaluated".format(models_evaluated, total_num_models))
+
+        mean, error_bar = get_error_bar(errors)
+        model = 'Kernel: ' + kernel +', C: ' + str(C) + ', epsilon: ' + str(epsilon)
+        write_to_results("{0}, error: {1} +- {2}".format(model, mean, error_bar))
+
+def evaluate_test():
+
+    x_test = data_provider.get_test_x()
+
+    clf = SVR(kernel='linear', C=0.41, epsilon=1)
+
+    print('fitting')
+    clf.fit(x_train, y_train)
+
+    
+
+
+    test_predictions = clf.predict(x_test)
+
+    submission = pd.read_csv('data/sample_submission.csv')
+    submission['time_to_failure'] = test_predictions
+    submission.to_csv("submission.csv", index=False)
+
+evaluate_test()
+
